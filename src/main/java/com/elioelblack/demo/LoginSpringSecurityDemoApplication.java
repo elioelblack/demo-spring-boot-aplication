a53803190413862940514e178ff12f68ef61e650
eliezer droid <eliohernandez94@gmail.com>
2020-04-21 16:34:56 -0600

    Initial commit

---
+++ src/main/java/com/elioelblack/demo/LoginSpringSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.elioelblack.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringSecurityDemoApplication.class, args);
+	}
+
+}
